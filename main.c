7dcd200ac440e91fca44b3a0cebb504da6010819
Allwin Williams <allwinwilliams.info@gmail.com>
2016-08-26 17:29:25 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  RatInAMaze
+//
+//  Created by Allwin Williams on 26/08/16.
+//
+//
+
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int i=0;
+    for (; i<10; i++) {
+        printf("s");
+    }
+    
+}
