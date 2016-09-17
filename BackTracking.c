d61fa3f7a3650e7aa323e17a4cf5b1746249e829
Allwin Williams <allwinwilliams.info@gmail.com>
2016-09-17 20:42:45 +0530

    backtracking working, tested
    made changes in backtracking, now working and tested

--- BackTracking.c
+++ BackTracking.c
@@ -23,14 +23,15 @@ int main()
     printf("enter size");
     scanf("%d%d",&m,&n);
     //Initializing mark - all value to 0.
-    for(i=1;i<=m;i++){
-        for(j=1;j<=n;j++){
-            mark[i][j]=0;
-        }
-    }
+    // for(i=1;i<=m;i++){
+    //     for(j=1;j<=n;j++){
+            
+    //     }
+    // }
     //Input for maze
     for(i=1;i<=m;i++){
         for(j=1;j<=n;j++){
+            mark[i][j]=0;
             scanf("%d",&maze[i][j]);
         }
     }
@@ -39,7 +40,7 @@ int main()
         maze[0][i]=1;
         maze[m+1][i]=1;
     }
-    //Outer wall - vertacal
+    //Outer wall - vertical
     for(i=0;i<=m+1;i++){
         maze[i][0]=1;
         maze[i][n+1]=1;
@@ -58,13 +59,12 @@ int main()
     push(1,1,2);
     
 
-    stack[0][0]=1;
-    stack[0][1]=1;
-    stack[0][2]=2;
-    top=0;
-
+    stack[0][0]=0;
+    stack[0][1]=0;
+    // stack[0][2]=2;
+    stack[0][2]=-1;
+    top=0;  
     while(s.top!=-1){
-
         InstStkI = pop();
         InstStkI.x = InstStkI.x + 1;
         InstStkI.y = InstStkI.y + 1;
@@ -72,27 +72,26 @@ int main()
         i=stack[top][0]+1;
         j=stack[top][1]+1;
         d=stack[top][2]+1;
-        top=top-1;
-        
+        top = top - 1;
         while(d<8){
             g = i + move[d][0];
             h = j + move[d][1];
             if( g == m && h == n ){
-                top = top+1;
+                top = top + 1;
                 stack[top][0] = i; stack[top][1] = j; stack[top][2] = d;
-                top = top+1;
-                stack[top][0] = m; stack[top][1] = n; stack[top][2] = 0;
+                top = top + 1;
+                stack[top][0] = m; stack[top][1] = n; stack[top][2] = 8;
                 push(i,j,d);
                 push(m,n,8);
                 for( p=0 ; p<=top ; p++){
                     printf("\nstack arr:  %d %d %d\n",stack[p][0],stack[p][1],stack[p][2]);
                     InstStkI = pop();
-                    printf("stack:  %d %d %d\n",InstStkI.x, InstStkI.y, InstStkI.dir);
+                    printf("stack:  %d %d %d\n",InstStkI.x, InstStkI.y, InstStkI.dir);    
                     //Print grid
                     usleep(900000);
                     for(i=0;i<=m+1;i++){
                         for(j=0;j<=n+1;j++){
-                            if(i==stack[p][0] && j==stack[p][1])
+                            if( i == stack[p][0] && j == stack[p][1] )
                                 printf(" X ");
                             else
                                 printf(" %d ",maze[i][j]);
@@ -104,7 +103,7 @@ int main()
                 printf("\n the path is \n");
                 for(i=0;i<=m+1;i++){
                     for(j=0;j<=n+1;j++){
-                        for( p=0 ; p<top ; p++){
+                        for( p=0 ; p<=top ; p++){
                             if(i==stack[p][0] && j==stack[p][1]){
                                 printf(" * ");
                                 break;
@@ -119,18 +118,15 @@ int main()
                 return 0;
             }
 
-            if( maze[g][h]==0 && mark[g][h]==0){
+            if( maze[g][h]==0 && mark[g][h]==0 ){
                 
                 mark[g][h]=1;
                 
-
                 push(i,j,d);
-                top = top+1;
+                top = top + 1;
                 stack[top][0]=i;
                 stack[top][1]=j;
                 stack[top][2]=d;
-                
-                
 
                 i=g;
                 j=h;
@@ -142,7 +138,6 @@ int main()
     printf("No path found");
     return 0;
 }
-
 void push (int x,int y,int dir)
 {
     if ( s.top > SIZE )
